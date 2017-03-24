ec696f529c6faab934eeb9dc8b2010bafd90416d
hezhihu <2854918124@qq.com>
2017-03-24 18:47:25 +0800

    基本配置 设置域名

--- src/main/java/com/sunO2/app/SunO2Application.java
+++ src/main/java/com/sunO2/app/SunO2Application.java
@@ -1,5 +1,7 @@
 package com.sunO2.app;
 
+import static spark.Spark.*;
+
 /**
  * Created by admin on 17/3/24.
  */
@@ -9,9 +11,15 @@ public class SunO2Application implements ApplicationWapper{
 
     public SunO2Application() {
       mContext = new SunO2Context();
+      initService();
     }
 
     public SunO2Context getContext() {
         return mContext;
+    }
+
+    private void initService() {
+        port(8081);
+        ipAddress("http://bbs.bugke.com");
     }
 }
